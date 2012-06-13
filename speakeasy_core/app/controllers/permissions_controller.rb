a6eda2d14890003b520330be3c0d95acb7a5c813
Mike Chlipala <mchlipala@gmail.com>
2012-06-13 09:51:13 -0400

    Minor refactorings to get the controller specs passing

--- speakeasy_core/app/controllers/permissions_controller.rb
+++ speakeasy_core/app/controllers/permissions_controller.rb
@@ -2,9 +2,8 @@ class PermissionsController < ApplicationController
   before_filter :find_room, :authenticate_user, :confirm_room_owner
 
   def create
-    find_invitee(params[:email])
     permission = @room.permissions.new(params[:permission])
-    permission.sid = @invitee["sid"]
+    permission.sid = invitee.sid
     if permission.save
       head status: :created, :location => [@room, permission]
     else
@@ -13,14 +12,16 @@ class PermissionsController < ApplicationController
   end
 
   def destroy
-    @room.permissions.where(id: params[:id]).first.destroy
+    #raise Permission.all.inspect
+    #raise @room.permissions.where(sid: invitee.sid).inspect
+    @room.permissions.where(sid: invitee.sid).first.destroy
     head status: :ok
   end
 
   private
 
-  def find_invitee(email)
-    @invitee ||= AuthService.get_user_by_email(email)
+  def invitee
+    @invitee ||= AuthService.get_user_by_email(params[:email])
   end
 
   def confirm_room_owner
