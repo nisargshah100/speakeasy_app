8b53676d9da23ffec5bee3cee89366d681c289c6
Daniel Kaufman <daniel.garrett.kaufman@gmail.com>
2012-06-14 18:48:48 -0400

    Adding KPIS

--- speakeasy_cheque/app/controllers/dashboards_controller.rb
+++ speakeasy_cheque/app/controllers/dashboards_controller.rb
@@ -6,16 +6,22 @@ class DashboardsController < ApplicationController
 
     @days = Rational(DAY_ZERO - DateTime.now).to_i
 
-    @recent_users = CreatedUser.where(:created_at.gt => DateTime.now - 2).count
-    @historical_users = CreatedUser.where(:created_at.lte => DateTime.now - 2).count
+    @recent_users = CreatedUser.where(:created_at.gt => DateTime.now - 1).count
+    @historical_users = CreatedUser.where(:created_at.lte => DateTime.now - 1).count
 
     @recent_users_per_day = @recent_users / 2
     @historical_users_per_day = @historical_users / @days
 
     @all_room_owners_count = CreatedRoom.where(:created_at.lte => DateTime.now).map { |room| room.sid }.uniq.count
-    @historical_room_owners_count = CreatedRoom.where(:created_at.lte => DateTime.now - 2).map { |room| room.sid }.uniq.count
+    @historical_room_owners_count = CreatedRoom.where(:created_at.lte => DateTime.now - 1).map { |room| room.sid }.uniq.count
 
     @all_percent_users_creating_room = (@all_room_owners_count.to_f / CreatedUser.count.to_f * 100).round(2)
     @historical_percent_users_creating_room = (@historical_room_owners_count.to_f / @historical_users.to_f * 100).round(2)
+
+    @recent_messages = CreatedMessage.where(:created_at.gt => DateTime.now - 1).count
+    @historical_messages = CreatedMessage.where(:created_at.lte => DateTime.now - 1).count
+
+    #Convert to active users
+    # @recent_messages_per_user
   end
 end
